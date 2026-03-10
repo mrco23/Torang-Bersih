5c32bfca439700c08ec373f9063918005a62aade
marcoismakalew <marcoismakalew@gmail.com>
2026-03-10 11:56:48 +0800

    feat: implement ArtikelRoute and register to app

--- be/app/api/routes/__init__.py
+++ be/app/api/routes/__init__.py
@@ -9,7 +9,7 @@ from app.api.routes.laporan_route import laporan_bp
 from app.api.routes.marketplace_route import marketplace_bp
 from app.api.routes.dashboard_route import dashboard_bp
 from app.api.routes.peta_route import peta_bp
-
+from app.api.routes.artikel_route import artikel_bp
 
 def register_routes(app: Flask) -> None:
     app.register_blueprint(auth_bp)
@@ -22,3 +22,4 @@ def register_routes(app: Flask) -> None:
     app.register_blueprint(marketplace_bp)
     app.register_blueprint(dashboard_bp)
     app.register_blueprint(peta_bp)
+    app.register_blueprint(artikel_bp)
