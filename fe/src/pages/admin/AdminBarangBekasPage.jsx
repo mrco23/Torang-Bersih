0ad14cc7afac8dcf6d2daa784e2979a54459dd5d
marcoismakalew <marcoismakalew@gmail.com>
2026-03-16 00:40:52 +0800

    resolve(AdminBarangBekas): import useCallback

--- fe/src/pages/admin/AdminBarangBekasPage.jsx
+++ fe/src/pages/admin/AdminBarangBekasPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { marketplaceAPI } from "../../services/api/routes/marketplace.route";
 import {
   KONDISI_LABELS,
